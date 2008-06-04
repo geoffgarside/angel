afcb9f2e33e49ade55e36d53dd0f6bd87252f728
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-04 12:04:35 +0100

    Change how Angel works. Now uses a symbolized hash of config options. Usage changes a bit.

--- lib/angel.rb
+++ lib/angel.rb
@@ -2,36 +2,92 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Angel
-  def self.set_pid_file(config)
-    unless config['pid_file'][0].chr == '/'
-      config['pid_file'] = File.join(config['cwd'], config['pid_file'])
+  def self.stop(server_type, *args)
+    self.send(:"stop_#{server_type}", options_from_array_of_hashes(args))
+  end
+  
+  def self.start(server_type, *args)
+    self.send(:"start_#{server_type}", options_from_array_of_hashes(args))
+  end
+  
+  def self.restart(server_type, *args)
+    self.send(:"restart_#{server_type}", options_from_array_of_hashes(args))    
+  end
+  
+  def self.options_from_array_of_hashes(args)
+    args.inject({}) { |options, hash| options.merge(hash) }
+  end
+  
+  def self.pid_file(*args)
+    options = options_from_array_of_hashes(args)
+    pid_file = options[:pid_file] || File.join(options[:cwd], "tmp/pids/server.pid")
+    
+    options[:port] ? pid_file.gsub(/\.pid$/, ".#{options[:port]}.pid") : pid_file
+  end
+  
+  def self.log_file(*args)
+    options = options_from_array_of_hashes(args)
+    log_file = options[:log_file] || File.join(options[:cwd], "log/server.log")
+    
+    options[:port] ? log_file.gsub(/\.log$/, ".#{options[:port]}.log") : log_file
+  end
+  
+  def self.setup(w)
+    # clean pid file before start if necessary
+    w.behavior(:clean_pid_file)
+
+    # determine state on god startup
+    w.transition(:init, { true => :up, false => :start }) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+      end
     end
 
-    config['pid_file'] = config['pid_file'].gsub(/\.pid$/, ".#{config['port']}.pid")
-  end
-
-  def self.set_log_file(config)
-    unless config['log_file'][0].chr == '/'
-      config['log_file'] = File.join(config['cwd'], config['log_file'])
+    # determine when process has finished starting
+    w.transition([:start, :restart], :up) do |on|
+      on.condition(:process_running) do |c|
+        c.running = true
+      end
+  
+      # failsafe
+      on.condition(:tries) do |c|
+        c.times = 5
+        c.transition = :start
+      end
     end
 
-    config['log_file'] = config['log_file'].gsub(/\.log$/, ".#{config['port']}.log") 
-  end
+    # start if process is not running
+    w.transition(:up, :start) do |on|
+      on.condition(:process_exits)
+    end
 
-  def self.process_name(config)
-    "#{config['app']}-cluster-#{config['port']}"
-  end
+    # Restart if memory or cpu usage is too high
+    w.transition(:up, :restart) do |on|
+      on.condition(:memory_usage) do |c|
+        c.interval = 20
+        c.above = 50.megabytes
+        c.times = [3,5] # three out of five intervals
+      end
+  
+      on.condition(:cpu_usage) do |c|
+        c.interval = 10
+        c.above = 10.percent
+        c.times = [3,5] # three out of five intervals
+      end
+    end
 
-  def self.start_cmd(config)
-    self.send(:"start_#{config['web_server']}", config)
-  end
-
-  def self.stop_cmd(config)
-    self.send(:"stop_#{config['web_server']}", config)
-  end
-
-  def self.restart_cmd(config)
-    self.send(:"restart_#{config['web_server']}", config)
+    # Life!!!
+    w.lifecycle do |on|
+      on.condition(:flapping) do |c|
+        c.to_state     = [:start, :restart]
+        c.times        = 5
+        c.within       = 5.minutes
+        c.transition   = :unmonitored
+        c.retry_in     = 10.minutes
+        c.retry_times  = 5
+        c.retry_within = 2.hours
+      end
+    end
   end
 end
 
